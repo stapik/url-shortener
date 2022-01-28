3f32ec339eaa27ad5faf91adddba1bc17388a31a
denfas112@gmail.com <GaCZyEfc5R5HLEx>
2022-01-28 15:33:34 +0300

    init db

---
+++ redirect/src/main/java/ru/stapik/us/redirect/Redirect.java
@@ -0,0 +1,12 @@
+package ru.stapik.us.redirect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Redirect {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Redirect.class, args);
+    }
+}
